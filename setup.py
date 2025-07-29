91aae9870a6ba38c63001be7f6205fa0391e1873
Lakshmi Prasanna Gunturi <gunturilakshmi.23s@gmail.com>
2025-07-28 19:36:10 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medicalchatbot',
+    version='0.1.0',
+    author='Prasanna', 
+    author_email='gunturilakshmi.23s@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
